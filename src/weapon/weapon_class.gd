777224d151dfba05f68f404d4a51ee418ab9fbc3
PremiumCesium <132301136+PremiumCesium@users.noreply.github.com>
2025-04-04 14:14:08 -0400

    Added Fire and Reload Methods

--- src/weapon/weapon_class.gd
+++ src/weapon/weapon_class.gd
@@ -1,4 +1,21 @@
-extends CharacterBody3D
+class_name Weapon
 
 @export var weapon_equipped: int
+@export var weapon_name: String
+@export var magazine_size: int
+@export var round_count: int
+
+# @icon("res://class_icon.png")
+
+func _fire():
+    if(round_count):
+        pass
+
+func _reload():
+    # Trigger animation
+
+    #Execute Reload
+    round_count = magazine_size
+
+    pass
 
