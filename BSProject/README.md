3d626c24124f4c0aa234c25824d9f15b93985f09
PremiumCesium <132301136+PremiumCesium@users.noreply.github.com>
2025-04-16 13:24:41 -0400

    Delete BSProject directory

--- BSProject/README.md
+++
@@ -1,2 +0,0 @@
-# BSProject
-Blossoming Studio's FPS Project
--- BSProject/src/enemy/BasicEnemy.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody3D
-class_name BasicEnemy
-
-func _physics_process(delta: float) -> void:
-	move_and_slide()
-	
-	#for moving around
-	#if velocity.length() > 0: 
-		#$animation.play("run")
